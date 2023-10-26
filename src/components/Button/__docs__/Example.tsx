171752debcf41f2990b43dfc9771fdadd50e8634
yutankaifa <1989541473@qq.com>
2023-10-26 13:40:19 +0800

    1

--- src/components/Button/__docs__/Example.tsx
+++ src/components/Button/__docs__/Example.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from "react";
-import Button, { ButtonProps } from "../Button";
+import Button, { ButtonProps } from "../index";
 
 const Example: FC<ButtonProps> = ({
   disabled = false,
